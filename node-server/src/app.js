49ecdf96652b470b800c8ba0fc302fc9573d98b2
Complexia <complexia701@gmail.com>
2023-06-19 00:10:52 +1000

    set up the whole thing as express

---
+++ node-server/src/app.js
@@ -0,0 +1,30 @@
+import process from 'process'
+import minimist from 'minimist'
+import { Web3Storage, getFilesFromPath } from 'web3.storage'
+import upload from './upload'
+
+// async function main () {
+//  let cid = await upload() 
+// }
+
+// main()
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route handler for the root path
+app.post('/upload', async (req, res) => {
+  try {
+    let cid = await upload(req);
+    // Send the response
+    res.send(cid);  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('An error occurred');
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
